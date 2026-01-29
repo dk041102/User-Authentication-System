2b90b8a30023703df96047bfb17c53e12e02da71
Deepak Kumar <116838434+dk041102@users.noreply.github.com>
2026-01-29 12:07:53 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const connectedDB = async() =>{
+    try{
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("MongoDB connected successfully");
+    }catch(err){
+        console.error(err);
+        process.exit(1);
+    }
+};
+module.exports = connectedDB;
